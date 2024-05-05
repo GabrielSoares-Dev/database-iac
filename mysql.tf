8ac6be961d411dfa2f2ff31189f575846bab8048
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-05 19:05:24 -0300

    fix: identifier

--- mysql.tf
+++ mysql.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "mysql_cluster" {
   allocated_storage                   = 20
-  identifier                          = "mysql_db_${lower(var.environment)}"
+  identifier                          = "mysql-${lower(var.environment)}"
   engine                              = "mysql"
   engine_version                      = "8.0.32"
   parameter_group_name                = "default.mysql8.0"
