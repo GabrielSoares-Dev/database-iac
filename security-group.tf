105e79df49e9046d19916961ba814e512ec5bc95
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-05 18:20:31 -0300

    fix: ingress rules

--- security-group.tf
+++ security-group.tf
@@ -3,6 +3,11 @@ resource "aws_security_group" "mysql_security_group" {
   description = "Security group to mysql db"
   vpc_id      = data.aws_vpc.vpc.id
 
+  ingress = {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "TCP"
+  }
   tags = {
     Name = "mysql_security_group"
   }
