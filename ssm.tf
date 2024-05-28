81732dbb3a40758e2445e47db357db92fa08ab7b
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-28 17:33:36 -0300

    feat: add username into ssm

--- ssm.tf
+++ ssm.tf
@@ -1,3 +1,14 @@
+resource "aws_ssm_parameter" "mysql_admin_username" {
+  name        = "/${lower(var.environment)}/database/mysql/username/admin"
+  description = "Admin mysql username"
+  type        = "String"
+  value       =  var.mysql_admin_username
+
+  tags = {
+    environment = lower(var.environment)
+  }
+}
+
 resource "aws_ssm_parameter" "mysql_admin_password" {
   name        = "/${lower(var.environment)}/database/mysql/password/admin"
   description = "Admin mysql password"
@@ -8,3 +19,4 @@ resource "aws_ssm_parameter" "mysql_admin_password" {
     environment = lower(var.environment)
   }
 }
+
