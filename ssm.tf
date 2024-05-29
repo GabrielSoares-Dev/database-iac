6ebf3434532b9274c63f44a2dc1901b6dbe8f15f
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-28 21:50:47 -0300

    feat: add ssm to vpc id

--- ssm.tf
+++ ssm.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "mysql_admin_username" {
   name        = "/${lower(var.environment)}/database/mysql/username/admin"
   description = "Admin mysql username"
   type        = "SecureString"
-  value       =  var.mysql_admin_username
+  value       = var.mysql_admin_username
 
   tags = {
     environment = lower(var.environment)
@@ -13,7 +13,7 @@ resource "aws_ssm_parameter" "mysql_admin_password" {
   name        = "/${lower(var.environment)}/database/mysql/password/admin"
   description = "Admin mysql password"
   type        = "SecureString"
-  value       =  random_password.mysql_password.result
+  value       = random_password.mysql_password.result
 
   tags = {
     environment = lower(var.environment)
